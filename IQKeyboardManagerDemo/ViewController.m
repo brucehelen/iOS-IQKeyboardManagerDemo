208cabaf95e713d2459716a74d90ea546cc911e6
zhengjing zhu <zhuzhengjing@gmail.com>
2015-07-08 11:46:42 +0800

    测试响应者链，view的userInteractionEnabled

--- IQKeyboardManagerDemo/ViewController.m
+++ IQKeyboardManagerDemo/ViewController.m
@@ -10,7 +10,7 @@
 #import "BZTextFieldView.h"
 #import "BZAllInOneView.h"
 
-@interface ViewController ()
+@interface ViewController () <UIGestureRecognizerDelegate>
 
 @end
 
@@ -35,9 +35,56 @@
     textField3.placeholder = @"测试3";
     [self.view addSubview:textField3];
     
-    BZAllInOneView *view = [[BZAllInOneView alloc] initWithFrame:CGRectMake(10, 200, 200, 200)];
-    [self.view addSubview:view];
+//    BZAllInOneView *view = [[BZAllInOneView alloc] initWithFrame:CGRectMake(10, 200, 200, 200)];
+//    [self.view addSubview:view];
+    
+    
+    
+    double delayInSeconds = 2.0;
+    dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, (int64_t)(delayInSeconds * NSEC_PER_SEC));
+    dispatch_after(popTime, dispatch_get_main_queue(), ^(void){
+        UIButton *button = [[UIButton alloc] initWithFrame:CGRectMake(10, 200, 100, 50)];
+        [button setTitle:@"点击我" forState:UIControlStateNormal];
+        [button addTarget:self action:@selector(buttonDidClicked:) forControlEvents:UIControlEventTouchUpInside];
+        button.backgroundColor = [[UIColor greenColor] colorWithAlphaComponent:0.5];
+        [self.view addSubview:button];
+        
+        UIImageView *imageView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, 30, 50)];
+        imageView.backgroundColor = [[UIColor redColor] colorWithAlphaComponent:0.8];
+        NSLog(@"imageView = %d", imageView.userInteractionEnabled);
+        [button addSubview:imageView];
+        
+        UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(35, 0, 30, 50)];
+        NSLog(@"label = %d", label.userInteractionEnabled);
+        label.backgroundColor = [[UIColor redColor] colorWithAlphaComponent:0.8];
+        [button addSubview:label];
+        
+        UIView *view = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 90, 50)];
+        // 默认为YES，设置为NO即可将消息传到下面的view
+        view.userInteractionEnabled = NO;
+        view.backgroundColor = [[UIColor yellowColor] colorWithAlphaComponent:0.6];
+        // single tap gesture recognizer
+        UITapGestureRecognizer *tapGestureRecognize = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(singleTapGestureRecognizer:)];
+        tapGestureRecognize.delegate = self;
+        tapGestureRecognize.numberOfTapsRequired = 1;
+        [view addGestureRecognizer:tapGestureRecognize];
+        [button addSubview:view];
+    });
 }
 
+- (void)singleTapGestureRecognizer:(UITapGestureRecognizer *)tapGesture
+{
+    NSLog(@"singleTapGestureRecognizer");
+}
+
+- (void)buttonDidClicked:(UIButton *)sender
+{
+    NSLog(@"clicked");
+}
+
+- (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event
+{
+    NSLog(@"touchesBegan = %@", event);
+}
 
 @end
